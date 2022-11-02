5893a0041fc63dfc25e45af363b2a6ef8b966066
poweredByED <pavel.pavlov.ed@gmail.com>
2022-11-02 18:54:22 +0300

    4.1. Разбор первой программы. [#159033 #105611]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Pavel Pavlov");
+        System.out.println("02.11.2022");
+    }
+}
